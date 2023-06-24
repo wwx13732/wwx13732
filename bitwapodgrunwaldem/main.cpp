941857406c6b205eb07343d69944b9f3d401552b
wwx13732 <wwx13732@student.wsb.warszawa.pl>
2023-06-24 17:57:53 +0200

    tworze dwa pliki

---
+++ bitwapodgrunwaldem/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int rok_obecny, rok_bitwy_pod_grunwaldem;
+
+int main()
+{
+    cout << "Podaj rok, a oblicze ile minelo od bitwy pod grunwaldem:" << endl;
+    cin >> rok_obecny
+    return 0;
+}
---
+++ cukierkowyprogram/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int uczniowie, cukierki, x, y;
+
+int main()
+{
+    cout << "Ilu uczniow jest w Twojej klasie:" << endl;
+    cin >> uczniowie;
+
+    cout << "Ile cukierkow kupila mama:";
+    cin >> cukierki;
+
+    x = cukierki/(uczniowie-1);
+    cout <<"Tyle cukierkow otrzyma kazdy uczen:" << x;
+
+    y = cukierki-x*(uczniowie-1);
+    cout << endl << "Tyle cukierkow zostanie Jasiowi:" << y;
+
+    return 0;
+}
