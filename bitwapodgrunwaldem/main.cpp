caf8f48955484816b1320f327aae70bb0b3f0d01
wwx13732 <wwx13732@student.wsb.warszawa.pl>
2023-06-24 18:04:10 +0200

    Update main.cpp

--- bitwapodgrunwaldem/main.cpp
+++ bitwapodgrunwaldem/main.cpp
@@ -7,6 +7,10 @@ int rok_obecny, rok_bitwy_pod_grunwaldem;
 int main()
 {
     cout << "Podaj rok, a oblicze ile minelo od bitwy pod grunwaldem:" << endl;
+<<<<<<< HEAD
     cin >> rok_nastêpny
+=======
+    cin >> rok_obecny
+>>>>>>> parent of fde6de5 (zmiana 1)
     return 0;
 }
